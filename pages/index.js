69bfd88b4041be0e82d2929b148770867475363c
Mateus de Campos <mateuscampos_nba@hotmail.com>
2025-12-07 20:57:41 +0000

    modifies copy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>As pessoas ficam icomodadas quando uma frase não termina como elas pirulito!</h1>
+    return <h1>As pessoas ficam icomodadas quando uma frase não termina como elas pirulito! 🍭</h1>
 }
 
 export default Home;
